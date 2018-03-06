1db68beb16e5169cbd41e5b9a4ce1925cde579b5
Evan Garrett <evang522@gmail.com>
2018-03-06 08:52:07 -0500

    Added actions from redux

---
+++ src/actions.js
@@ -0,0 +1,20 @@
+export const SET_FEEDBACK = 'SET_FEEDBACK';
+export const setFeedback = () => ({
+    type:SET_FEEDBACK,
+})
+
+export const ADD_GUESS = 'ADD_GUESS';
+export const addGuess = (guess) => {
+    type:ADD_GUESS,
+    guess
+}
+
+export const RESET_GAME = 'RESET_GAME'
+export const resetGame = () => {
+    type:RESET_GAME
+}
+
+export const TOGGLE_MODAL = 'TOGGLE_MODAL'
+export const toggleModal = () => {
+    
+}
